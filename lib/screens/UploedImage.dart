f09e16b3a36df0085f1e20871d7f710845b140bc
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2022-11-10 19:17:13 +0300

    solve the issue

--- lib/screens/UploedImage.dart
+++ lib/screens/UploedImage.dart
@@ -1,118 +1,118 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:image_picker/image_picker.dart';
 
-class UploadImage extends StatefulWidget {
-  const UploadImage({super.key});
+// class UploadImage extends StatefulWidget {
+//   const UploadImage({super.key});
 
-  @override
-  State<UploadImage> createState() => _UploadImageState();
-}
+//   @override
+//   State<UploadImage> createState() => _UploadImageState();
+// }
 
-class _UploadImageState extends State<UploadImage> {
-  File? image;
-  Future _pickImage1() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+// class _UploadImageState extends State<UploadImage> {
+//   File? image;
+//   Future _pickImage1() async {
+//     try {
+//       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
-      if (image == null) return;
+//       if (image == null) return;
 
-      final imageTemp = File(image.path);
+//       final imageTemp = File(image.path);
 
-      setState(() => this.image = imageTemp);
-    } on PlatformException catch (e) {
-      print('Failed to pick image: $e');
-    }
-  }
+//       setState(() => this.image = imageTemp);
+//     } on PlatformException catch (e) {
+//       print('Failed to pick image: $e');
+//     }
+//   }
 
-  Future _pickImage2() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+//   Future _pickImage2() async {
+//     try {
+//       final image = await ImagePicker().pickImage(source: ImageSource.camera);
 
-      if (image == null) return;
+//       if (image == null) return;
 
-      final imageTemp = File(image.path);
+//       final imageTemp = File(image.path);
 
-      setState(() => this.image = imageTemp);
-    } on PlatformException catch (e) {
-      print('Failed to pick image: $e');
-    }
-  }
+//       setState(() => this.image = imageTemp);
+//     } on PlatformException catch (e) {
+//       print('Failed to pick image: $e');
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "voygers",
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        elevation: 0,
-        backgroundColor: Color.fromARGB(144, 7, 211, 20),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () => Navigator.pop(context, "trip_page"),
-            icon: Icon(
-              Icons.app_registration,
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        width: 600,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            InkWell(
-              child: image != null
-                  ? Image.file(
-                      image!,
-                      width: 200,
-                      height: 200,
-                    )
-                  : Image.network(
-                      "https://flyclipart.com/thumb2/purple-camera-clip-art-594601.png",
-                      width: 200,
-                      height: 200,
-                    ),
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text("Upload image from"),
-                        content: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            IconButton(
-                                onPressed: () {
-                                  _pickImage1();
-                                },
-                                icon: Icon(Icons.browse_gallery)),
-                            IconButton(
-                                onPressed: () {
-                                  _pickImage2();
-                                },
-                                icon: Icon(Icons.camera))
-                          ],
-                        ),
-                      );
-                    });
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Upload"),
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text(
+//           "voygers",
+//           style: TextStyle(
+//               fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+//         ),
+//         elevation: 0,
+//         backgroundColor: Color.fromARGB(144, 7, 211, 20),
+//         actions: <Widget>[
+//           IconButton(
+//             onPressed: () => Navigator.pop(context, "trip_page"),
+//             icon: Icon(
+//               Icons.app_registration,
+//               color: Colors.white,
+//             ),
+//           )
+//         ],
+//       ),
+//       body: Container(
+//         width: 600,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             InkWell(
+//               child: image != null
+//                   ? Image.file(
+//                       image!,
+//                       width: 200,
+//                       height: 200,
+//                     )
+//                   : Image.network(
+//                       "https://flyclipart.com/thumb2/purple-camera-clip-art-594601.png",
+//                       width: 200,
+//                       height: 200,
+//                     ),
+//               onTap: () {
+//                 showDialog(
+//                     context: context,
+//                     builder: (context) {
+//                       return AlertDialog(
+//                         title: Text("Upload image from"),
+//                         content: Row(
+//                           mainAxisAlignment: MainAxisAlignment.center,
+//                           children: [
+//                             IconButton(
+//                                 onPressed: () {
+//                                   _pickImage1();
+//                                 },
+//                                 icon: Icon(Icons.browse_gallery)),
+//                             IconButton(
+//                                 onPressed: () {
+//                                   _pickImage2();
+//                                 },
+//                                 icon: Icon(Icons.camera))
+//                           ],
+//                         ),
+//                       );
+//                     });
+//               },
+//             ),
+//             ElevatedButton(
+//               onPressed: () {},
+//               child: Text("Upload"),
+//               style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
